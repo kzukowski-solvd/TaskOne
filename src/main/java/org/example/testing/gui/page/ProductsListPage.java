bc08c06f69602f98d96d9551978f770d545f668c
kacperzukowski <kzukowski@solvd.com>
2023-05-23 08:46:49 +0200

    added abstraction

--- src/main/java/org/example/testing/gui/page/ProductsListPage.java
+++ src/main/java/org/example/testing/gui/page/ProductsListPage.java
@@ -1,15 +1,20 @@
 package org.example.testing.gui.page;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ProductsListPage extends AbstractPage{
+public class ProductsListPage extends AbstractPage {
+
     @FindBy(css = ".s-result-item")
     private List<WebElement> products;
+    @FindBy(css = "#s-result-sort-select option[value*='price-desc-rank']")
+    private WebElement priceDescendingOption;
 
     public ProductsListPage(WebDriver driver) {
         super(driver);
@@ -20,12 +25,55 @@ public class ProductsListPage extends AbstractPage{
         return products.isEmpty();
     }
 
-    public void printProductTitles() {
+    public ArrayList<String> getProductTitles() {
         List<WebElement> productTitles = driver.findElements(By.cssSelector(".a-size-medium.a-color-base.a-text-normal"));
-        int i = 1;
+        ArrayList<String> titles = new ArrayList<>();
         for (WebElement title : productTitles) {
-            System.out.println(i++ + ". " + title.getText());
+            titles.add(title.getText());
         }
+        return titles;
     }
+
+    public String getProductTitle(int index) {
+        return getProductTitles().get(index);
+    }
+
+    public void filterByPriceDescending() {
+        click(priceDescendingOption);
+    }
+
+    public ArrayList<Double> getProductPrices() {
+        List<WebElement> productPrices = driver.findElements(By.cssSelector(".a-price-whole"));
+        ArrayList<Double> prices = new ArrayList<>();
+        for (WebElement price : productPrices) {
+            prices.add(Double.parseDouble(price.getText().replace(",", "")));
+        }
+        return prices;
+    }
+    public Double getProductPrice(int index) {
+        return getProductPrices().get(index);
+    }
+
+    public ArrayList<String> getProductLinks() {
+
+        List<WebElement> productLinks = driver.findElements(By.cssSelector("a.a-link-normal"));
+        ArrayList<String> links = new ArrayList<>();
+        for (WebElement link : productLinks) {
+            links.add(link.getAttribute("href"));
+        }
+        // return a links without duplicates and filter out ones containing #customerReviews
+        return new ArrayList<>(links).stream().filter(link -> !link.contains("#customerReviews")).distinct()
+                .collect(java.util.stream.Collectors.toCollection(ArrayList::new));
+    }
+
+    public String getProductLink(int index) {
+        return getProductLinks().get(index);
+    }
+
+    public ProductPage openProductByLink(String link) {
+        driver.get(link);
+        return new ProductPage(driver);
+    }
+
 
 }
